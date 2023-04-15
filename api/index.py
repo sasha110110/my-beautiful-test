81b1eadb5ddf2a1a8530ae405b8fb79a3aaeaf92
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-16 00:59:58 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,31 @@
 from flask import Flask
 
 app = Flask(__name__)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+
+TOKEN = "5650199850:AAFVpNnH9pLBXQkomn-nJZlBnNucjP4s3sQ"
+update=telegram.Update
+updater = Updater(TOKEN, use_context=True)
+bot=telegram.Bot(TOKEN)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
-
 @app.route('/test')
 def test():
-    return 'test'
+    chat_id="@1093497662"# msg.sender_chat["username"]
+    bot.sendMessage(chat_id=chat_id, text="test")
+    return "Done"
+
+@app.route("/hook", methods=['POST', "GET"])
+def hook():
+   if request.method == "POST":
+       content = json.loads(request.get_data())# #WORKING
+       
+        #chat_id = request.json["message"]["chat"]["id"]
+       chat_id="@1093497662"# msg.sender_chat["username"]
+       bot.sendMessage(chat_id=chat_id, text=str(content))
+       return 'ok'
+    
