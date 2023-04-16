a9d2a4ea6cb3265738af15bb43b923c3573b8515
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-16 10:40:53 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,9 +18,9 @@ def home():
 
 @app.route('/test')
 def test():
-    #chat_id="1093497662"# msg.sender_chat["username"]
-    #bot.sendMessage(chat_id=chat_id, text="test")
-    return "SMTH"
+    chat_id="1093497662"# msg.sender_chat["username"]
+    bot.sendMessage(chat_id=chat_id, text="test")
+    
   
 
 @app.route("/hook", methods=['POST', "GET"])
