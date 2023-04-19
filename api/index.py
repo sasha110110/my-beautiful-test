6e4c0775feb325388f6f79d1779c18b39437eb4c
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-19 11:21:57 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
 import json
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
+from telegram import Bot
+from telegram.ext import Updater , CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+import os
+from telegram import InlineKeyboardButton, ReplyKeyboardMarkup, Update, InlineKeyboardMarkup
 
 TOKEN = "5650199850:AAFVpNnH9pLBXQkomn-nJZlBnNucjP4s3sQ"
 app = Flask(__name__)
@@ -11,6 +14,33 @@ context=telegram.ext.CallbackContext
 
 updater = Updater(TOKEN, use_context=True)
 bot=telegram.Bot(TOKEN)
+updater = Updater(TOKEN)
+dp = updater.dispatcher
+
+CHAT_TO = ""
+
+def help(update, context):
+    global CHAT_TO
+    CHAT_TO=update.message.chat_id
+    text="Я бот простого поиска. Ищу по ключевым словам и темам на сайте и в туториале. Просто напиши что искать"
+    context.bot.sendMessage(text=text, chat_id=CHAT_TO)
+def greet(update, context):
+    global CHAT_TO
+    CHAT_TO=update.message.chat_id
+    text="Hiiiii"
+    context.bot.sendMessage(text=text, chat_id=CHAT_TO)context.bot.sendMessage(text=text, chat_id=CHAT_TO)
+    
+proc_handler= MessageHandler(Filters.text & (~Filters.command) , process_msg)
+greet_handler=CommandHandler("start", greet)
+#query_handler=CallbackQueryHandler(first_choice)
+
+   
+dp.add_handler(proc_handler)
+dp.add_handler(greet_handler)
+#dp.add_handler(query_handler, group=1)
+    
+
+    
 
 @app.route('/')
 def home():
@@ -26,11 +56,12 @@ def test():
 @app.route("/"+TOKEN, methods=['POST'])
 def hook():
    if request.method == "POST":
-       content = json.loads(request.get_data())# #WORKING
-       print(content)
+       dp.process_update(update) #dp.processUpdate(update)
+       #content = json.loads(request.get_data())# #WORKING
+       #print(content)
        
-        #chat_id = request.json["message"]["chat"]["id"]
-       chat_id="1093497662"# msg.sender_chat["username"]
-       bot.sendMessage(chat_id=chat_id, text=str(content))
+        ##chat_id = request.json["message"]["chat"]["id"]
+       #chat_id="1093497662"# msg.sender_chat["username"]
+       #bot.sendMessage(chat_id=chat_id, text=str(content))
    return "ok"
     
