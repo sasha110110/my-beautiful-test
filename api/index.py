7c1fe21a68bf1c6d91e5b2573eaf2717d3819a16
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-26 08:15:48 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,11 +13,11 @@ from io import StringIO
 #base_dir = os.path.dirname(os.path.abspath(__file__))
 
 #f=open(os.path.join("data", "df_all_articles.csv"))
-data=open("df_all_articles.csv")
-li = data.read()#.split('\n')
-df_articles=pd.read_csv(StringIO(li))
+#data=open("df_all_articles.csv")
+#li = data.read()#.split('\n')
+#df_articles=pd.read_csv(StringIO(li))
     
-#df_tutorial = pd.read_csv("FV_tutorial.csv")
+df_tutorial = pd.read_csv("FV_tutorial.csv")
 #df_articles = pd.DataFrame(df_list, columns=["Q", "link", "category"])
 
 GLOBAL_SEARCH=""
