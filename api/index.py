98ffa225aef367408fbdbcdaf1976b3014b12ad5
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-05-04 16:02:08 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -66,7 +66,7 @@ def test():
 @app.route('/check')
 def check():
     global df_articles
-    bot.sendMessage(chat_id="1093497662", text=df_articles.head(2).values) 
+    bot.sendMessage(chat_id="1093497662", text=str(df_articles.head(2).values))
     return "ok"
 
 @app.route("/"+TOKEN, methods=['POST'])
